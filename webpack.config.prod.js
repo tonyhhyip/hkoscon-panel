67936429a6af7c92276aaffe73cc4a1ccc4d00a2
Tony Yip <tony@opensource.hk>
2017-01-15 04:54:42 +0800

    Add webpack production config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const UglifyPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
 const merge = require('webpack-merge');
 const config = require('./webpack.config.base');
 
@@ -8,6 +9,12 @@ module.exports = merge.smart({
   plugin: [
     new UglifyPlugin({
       compress: true
+    }),
+    new DefinePlugin({
+      'process.env': {
+        NODE_ENV: 'production'
+      }
     })
-  ]
+  ],
+  devtool: 'source-map'
 }, config);
