642ba24466d460f37227e46842d40e6c001e33d7
Tony Yip <tony@opensource.hk>
2017-01-16 00:56:48 +0800

    Remove webpack dev server only setting

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,17 +1,8 @@
 'use strict';
 
-const HotModuleReplacementPlugin = require('webpack/lib/HotModuleReplacementPlugin');
 const merge = require('webpack-merge');
 const config = require('./webpack.config.base');
 
 module.exports = merge.smart({
-  devtool: 'eval-source-map',
-  plugin: [
-    new HotModuleReplacementPlugin()
-  ],
-  entry: {
-    app: [
-      'webpack-dev-server/client?http://localhost:8080/'
-    ]
-  }
+  devtool: 'eval-source-map'
 }, config);
