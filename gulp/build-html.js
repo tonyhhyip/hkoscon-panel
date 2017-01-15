edf7cea5724f95108582aa910c616688f6160e2c
Tony Yip <tony@opensource.hk>
2017-01-15 23:22:03 +0800

    Change build to SPA

--- gulp/build-html.js
+++ gulp/build-html.js
@@ -2,20 +2,14 @@
 
 const gulp = require('gulp');
 const {log} = require('gulp-util');
-const {FileSystemLoader, Environment} = require('nunjucks');
 const jinja = require('gulp-nunjucks');
 const rename = require('gulp-rename');
 const htmlmin = require('gulp-htmlmin');
 
-const env = new Environment([
-  new FileSystemLoader('assets/html/page'),
-  new FileSystemLoader('assets/html/layout')
-]);
-
 gulp.task('build:html', () => {
   return gulp.src('./assets/html/page/**/*.jinja')
     .on('error', log)
-    .pipe(jinja.compile({}, {env}))
+    .pipe(jinja.compile({}))
     .pipe(rename({
       extname: '.html'
     }))
--- gulp/dev-html.js
+++ gulp/dev-html.js
@@ -2,19 +2,13 @@
 
 const gulp = require('gulp');
 const {log} = require('gulp-util');
-const {FileSystemLoader, Environment} = require('nunjucks');
 const jinja = require('gulp-nunjucks');
 const rename = require('gulp-rename');
-
-const env = new Environment([
-  new FileSystemLoader('assets/html/page', {watch: true}),
-  new FileSystemLoader('assets/html/layout', {watch: true})
-]);
 
 gulp.task('dev:html', () => {
   return gulp.src('./assets/html/page/**/*.jinja')
     .on('error', log)
-    .pipe(jinja.compile({}, {env}))
+    .pipe(jinja.compile({}))
     .pipe(rename({
       extname: '.html'
     }))
