f900615b244a5bb3527ebf2ba10ab7e8cd117e4a
Tony Yip <tony@opensource.hk>
2017-01-17 02:35:08 +0800

    Add clock component

---
+++ assets/js/components/Clock.jsx
@@ -0,0 +1,29 @@
+//@flow
+import React from 'react';
+import moment from 'moment';
+type State = {
+  time: string
+}
+export default class Clock extends React.Component {
+
+  state: State;
+
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      time: 'loading'
+    };
+  }
+
+  render() {
+    return <span {...this.props}>{this.state.time}</span>;
+  }
+
+  componentDidMount() {
+    setInterval(function () {
+      this.setState({
+        time: moment().format('hh:mm:ss')
+      });
+    }, 500);
+  }
+}
