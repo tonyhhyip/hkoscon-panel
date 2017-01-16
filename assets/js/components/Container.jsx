7f9d4a968664c431faf9048f1297af07fc8618de
Tony Yip <tony@opensource.hk>
2017-01-17 02:34:20 +0800

    Add Container element

---
+++ assets/js/components/Container.jsx
@@ -0,0 +1,13 @@
+//@flow
+import React from 'react';
+import classNames from 'classnames';
+
+type Props = {
+  className?: string,
+  children: React.Element<*>
+}
+
+export default function Container(props: Props) {
+  const className = classNames(props.className, 'container');
+  return <div className={className} {...props}>{props.children}</div>;
+}
