447bb0f01765e2dd08321bd63ef3a32e00102908
Tony Yip <tony@opensource.hk>
2017-01-17 00:50:06 +0800

    Add flow declare for sse-nodejs

--- flow-typed/global/sse.js
+++ flow-typed/global/sse.js
@@ -1,7 +1,8 @@
 declare class Sse {
   resp: http.OutgoingMessage;
   sendEvent(event: string, data: any, time?: number): void;
+  disconnect(callback: Function): void;
 }
 declare module 'sse-nodejs' {
-
+  declare var exports: (response: Restify$Response) => Sse;
 }
