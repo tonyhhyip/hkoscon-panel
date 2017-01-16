87a1f4b6b8696795fee7eba8cadd7efc7f7de9c6
Tony Yip <tony@opensource.hk>
2017-01-17 02:34:34 +0800

    Add row component

---
+++ assets/js/components/Row.jsx
@@ -0,0 +1,13 @@
+//@flow
+import React from 'react';
+import classNames from 'classnames';
+
+type Props = {
+  className?: string,
+  children: React.Element<*>
+}
+
+export default function Row(props: Props) {
+  const className = classNames(props.className, 'row');
+  return <div className={className} {...props}>{props.children}</div>
+};
