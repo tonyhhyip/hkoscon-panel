3b00fdfac28c752f568763a057ff17ae3a2fd961
Tony Yip <tony@opensource.hk>
2017-01-21 03:18:23 +0800

    Update flow type

--- flow-typed/global/sse.js
+++ flow-typed/global/sse.js
@@ -1,8 +1,9 @@
+import type expreess from 'express';
 declare class Sse {
-  resp: http.OutgoingMessage;
+  resp: expreess.$Response;
   sendEvent(event: string, data: any, time?: number): void;
   disconnect(callback: Function): void;
 }
 declare module 'sse-nodejs' {
-  declare var exports: (response: Restify$Response) => Sse;
+  declare var exports: (response: expreess.$Response) => Sse;
 }
