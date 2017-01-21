9036e44d1559685e331d981fdef34f81628b6493
Tony Yip <tony@opensource.hk>
2017-01-22 01:04:34 +0800

    Add sse notice capture

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 //@flow
 'use strict';
+import './notice';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './Router';
---
+++ assets/js/notice.js
@@ -0,0 +1,7 @@
+'use strict';
+/* global toastr */
+const source = new EventSource('/sse');
+source.addEventListener('check-in', function (event) {
+  const data = JSON.parse(event.data);
+  toastr.info(`A ${data.type} ${data.name} has check-in`);
+});
