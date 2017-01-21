f718ea91269d236083eee4d7b8c0fc772227d37d
Tony Yip <tony@opensource.hk>
2017-01-22 01:03:21 +0800

    Add Webcam checkin function

--- assets/js/Router.jsx
+++ assets/js/Router.jsx
@@ -1,23 +1,15 @@
 //@flow
 'use strict';
 import React from 'react';
-import { Redirect, IndexRoute, IndexRedirect, browserHistory, Router, Route } from 'react-router';
+import { Redirect, IndexRedirect, browserHistory, Router, Route } from 'react-router';
 import Container from './Container';
 import Dashboard from './pages/Dashboard';
 import Attendees from './pages/Attendees';
-
-type Props = {
-  children: React.Element<*>
-}
-
-function EmptyRoute(props: Props) {
-  return <div>{props.children}</div>
-}
+import CheckIn from './pages/CheckIn';
 
 type RouteProps = {
   data: Object;
 }
-
 
 export default function AppRouter(props: RouteProps) {
   return (
@@ -26,6 +18,7 @@ export default function AppRouter(props: RouteProps) {
         <IndexRedirect to="/dashboard"/>
         <Route path="/dashboard" component={Dashboard} />
         <Route component={Attendees} path="/dashboard/attendees" />
+        <Route component={CheckIn} path="/dashboard/check-in" />
         <Redirect to="/dashboard" from="*"/>
       </Route>
     </Router>
---
+++ assets/js/pages/CheckIn.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import QrCode from 'qrcode-reader';
+import Container from '../components/Container';
+import extract from '../barcode';
+
+type State = {
+  timeout: number | null,
+  stream: Object | null,
+  capture: boolean
+}
+
+export default class CheckIn extends React.Component {
+  state: State;
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeout: null,
+      stream: null,
+      capture: true
+    };
+  }
+  render() {
+    return (
+      <Container style={{textAlign: 'center'}}>
+        <video width="500px" ref="video" />
+        <canvas width="498px" height="375px" style={{display: 'none'}} ref="canvas" id="qr-canvas" />
+        <Container>
+          <button className="btn" onClick={this.toggleCapture.bind(this)}>
+            {this.state.capture ? 'Stop' : 'Start'} Capture
+          </button>
+        </Container>
+      </Container>
+    )
+  }
+
+  componentDidMount() {
+    this.startCapture();
+  }
+
+  componentWillUnmount() {
+    this.stopCapture();
+  }
+
+  toggleCapture() {
+    if (this.state.capture) {
+      this.stopCapture();
+    } else {
+      this.startCapture();
+    }
+  }
+
+  startCapture() {
+    const self = this;
+    const video = this.refs.video;
+    const canvas = this.refs.canvas;
+    const context = canvas.getContext('2d');
+    let localMediaStream;
+
+    const qrcode = new QrCode();
+
+    const scan = function () {
+      if (localMediaStream) {
+        context.drawImage(video, 0, 0, 307, 250);
+        try {
+          qrcode.decode();
+        } catch (e) {
+          console.error(e);
+        }
+        self.setState({
+          timeout: setTimeout(scan, 500)
+        });
+      } else {
+        self.setState({
+          timeout: setTimeout(scan, 500)
+        });
+      }
+    };
+
+    const successCallback = function (stream) {
+      video.src = window.URL.createObjectURL(stream) || stream;
+      localMediaStream = stream;
+      self.setState({stream, timeoute: setTimeout(scan, 1000)});
+      video.play();
+    };
+
+    navigator.getUserMedia({video: true}, successCallback, function (e) {
+      console.error(e);
+    });
+
+    qrcode.callback = function (result) {
+      if (result)
+        self.handleTicketScan(result);
+    };
+
+    this.setState({capture: true});
+  }
+
+  handleTicketScan(result) {
+    const {attendee} = extract(result);
+    fetch(`/api/checkin/${attendee}`, {method: 'POST'})
+      .then((response) => {
+        /* global toastr */
+        if (response.status === 200) {
+          toastr.success('Success Check In');
+        } else {
+          toastr.error('Fail to check in');
+        }
+      })
+  }
+
+  stopCapture() {
+    if (this.state.stream) {
+      this.state.stream.getVideoTracks().forEach((track) => {
+        track.stop();
+      });
+    }
+
+    if (this.state.timeout) {
+      clearTimeout(this.state.timeout);
+    }
+
+    this.setState({capture: false});
+  }
+
+}
