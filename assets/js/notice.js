435717729971624878195c21774de0fdec7cd98e
Tony Yip <tony@opensource.hk>
2017-01-26 14:36:18 +0800

    Add argument for dispatch in coming up

--- assets/js/notice.js
+++ assets/js/notice.js
@@ -1,7 +1,10 @@
 'use strict';
 import toastr from 'toastr';
-const source = new EventSource('/sse');
-source.addEventListener('check-in', function (event) {
-  const data = JSON.parse(event.data);
-  toastr.info(`A ${data.type} ${data.name} has check-in`);
-});
+
+export default function (store) {
+  const source = new EventSource('/sse');
+  source.addEventListener('check-in', function (event) {
+    const data = JSON.parse(event.data);
+    toastr.info(`A ${data.type} ${data.name} has check-in`);
+  });
+}
