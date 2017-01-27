baf2a61e19f46591560402c8828bba204954ad13
Tony Yip <tony@opensource.hk>
2017-01-28 00:14:27 +0800

    Add event dispatch for notice

--- assets/js/notice.js
+++ assets/js/notice.js
@@ -1,10 +1,12 @@
 'use strict';
 import toastr from 'toastr';
+import {updateCheckIn} from './redux/action';
 
 export default function (store) {
   const source = new EventSource('/sse');
   source.addEventListener('check-in', function (event) {
     const data = JSON.parse(event.data);
     toastr.info(`A ${data.type} ${data.name} has check-in`);
+    store.dispatch(updateCheckIn(data.id, true));
   });
 }
