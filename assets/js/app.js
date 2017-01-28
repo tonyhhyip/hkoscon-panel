f3ce3ddad3b1f49a1e3db8a803ee6a6ae70fa98a
Tony Yip <tony@opensource.hk>
2017-01-28 22:53:25 +0800

    Register Service Worker

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,21 @@
 import notice from './notice';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 import AppRouter from './Router';
 import createStore from './redux/store';
 import fetchData from './fetch';
+
+runtime.register({
+  scope: `https://${location.host}/`
+})
+  .catch(e => console.log(e));
 
 Promise.all(fetchData)
   .then((values) => createStore(values))
   .then(function (data) {
     ReactDOM.render(<AppRouter data={data} />, document.getElementById('react-root'));
     notice(data);
+    return data;
   });
 
