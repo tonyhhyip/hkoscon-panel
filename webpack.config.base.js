7b9f6c15042c16e8300517a2d67a0c3a9dc81154
Tony Yip <tony@opensource.hk>
2017-01-28 22:52:38 +0800

    Add service worker build in webpack

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -3,12 +3,22 @@
 const OccurenceOrderPlugin = require('webpack/lib/optimize/OccurrenceOrderPlugin');
 const DedupePlugin = require('webpack/lib/optimize/DedupePlugin');
 const AggressiveMergingPlugin = require('webpack/lib/optimize/AggressiveMergingPlugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 
 module.exports = {
   plugins: [
     new OccurenceOrderPlugin(),
     new DedupePlugin(),
-    new AggressiveMergingPlugin()
+    new AggressiveMergingPlugin(),
+    new ServiceWorkerWebpackPlugin({
+      entry: `${__dirname}/assets/js/sw.js`,
+      excludes: [
+        '**/.*',
+        '**/*.map',
+        '*.html'
+      ],
+      publicPath: '/assets/'
+    })
   ],
   externals: {
     jquery: 'jQuery',
