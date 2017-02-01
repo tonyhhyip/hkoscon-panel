b36aa1d4614d8160fd3dd3126eaf7a5b27bde362
Tony Yip <tony@opensource.hk>
2017-02-02 05:58:29 +0800

    Fix typo and replacement error

--- src/eventbrite.js
+++ src/eventbrite.js
@@ -3,11 +3,16 @@
 const url = require('url');
 const express = require('express');
 const checkIn = require('./handlers/checkin');
+const debug = require('debug')('router:eventbrite');
 const router = express.Router();
 
 router.post('/check-in', function (req, res) {
   const path = url.parse(req.body.api_url).pathname;
-  const id = path.repalce(/^\/|\/$/, '').split('/').pop();
+  let id = null;
+  const pieces = path.replace(/^\/|\/$/, '').split('/');
+  while (!id) {
+    id = pieces.pop();
+  }
   checkIn(req, res, id);
 });
 
