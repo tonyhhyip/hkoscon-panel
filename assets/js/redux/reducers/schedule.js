d8b859bc95d4b8d932e7341535cb49b9d222068e
Tony Yip <tony@opensource.hk>
2017-02-03 01:55:32 +0800

    Fix flow parameter type error

--- assets/js/redux/reducers/schedule.js
+++ assets/js/redux/reducers/schedule.js
@@ -1,7 +1,7 @@
 //@flow
 import {IMPORT_TIMETABLE} from '../action';
 
-export default function (state: Object = [], action: Object) {
+export default function (state: Array<Object> = [], action: Object) {
   if (action.type === IMPORT_TIMETABLE && action.module === 'schedule') {
     return [].concat(state, action.data);
   } else {
