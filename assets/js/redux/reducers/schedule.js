3f7ff2efef80350e4cd8d954ec3fc97eb13f95f1
Tony Yip <tony@opensource.hk>
2017-02-02 22:37:04 +0800

    Fix schedule data type

--- assets/js/redux/reducers/schedule.js
+++ assets/js/redux/reducers/schedule.js
@@ -1,9 +1,9 @@
 //@flow
 import {IMPORT_TIMETABLE} from '../action';
 
-export default function (state: Object = {}, action: Object) {
+export default function (state: Object = [], action: Object) {
   if (action.type === IMPORT_TIMETABLE && action.module === 'schedule') {
-    return Object.assign({}, state, action.data);
+    return [].concat(state, action.data);
   } else {
     return state;
   }
