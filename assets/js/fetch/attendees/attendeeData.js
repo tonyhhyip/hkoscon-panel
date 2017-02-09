5930f85588a29d95c9fb91a43501e7bc6024c2e4
Tony Yip <tony@opensource.hk>
2017-02-09 22:31:01 +0800

    Update data structure

--- assets/js/fetch/attendees/attendeeData.js
+++ assets/js/fetch/attendees/attendeeData.js
@@ -2,17 +2,6 @@ import {database} from '../../firebase';
 
 const promise = new Promise((resolve) => {
   database.ref('attendees').once('value', snapshot => resolve(snapshot.val()));
-})
-  .then(function (data) {
-    return Object.keys(data).map(function (id) {
-      const attendee = data[id];
-      return {
-        id,
-        name: attendee.name,
-        ticket: attendee.ticket,
-        type: attendee.type
-      }
-    });
-  });
+});
 
 export default promise;
