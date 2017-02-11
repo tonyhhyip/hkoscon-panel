6134895b8a38763d751d92509141a22384048ac2
Tony Yip <tony@opensource.hk>
2017-02-11 18:00:19 +0800

    optimize code of database reference

--- assets/js/fetch/attendees/attendeeStatus.js
+++ assets/js/fetch/attendees/attendeeStatus.js
@@ -4,20 +4,24 @@ import {importAttendeeCheckin, updateCheckIn} from '../../action';
 
 export default function (store) {
   const date = moment().format('YYYYMMDD');
-  const key = `checkIn/${date}`;
-  database.ref(key).once('value', (snapshot) => {
+  const ref = database.ref(`checkIn/${date}`);
+
+  ref.once('value', (snapshot) => {
     const action = importAttendeeCheckin(snapshot.val());
     store.dispatch(action);
   });
-  database.ref(key).on('child_added', (data) => {
+
+  ref.on('child_added', (data) => {
     const action = updateCheckIn(data.key, data.val());
     store.dispatch(action);
   });
-  database.ref(key).on('child_changed', (data) => {
+
+  ref.on('child_changed', (data) => {
     const action = updateCheckIn(data.key, data.val());
     store.dispatch(action);
   });
-  database.ref(key).on('child_removed', (data) => {
+
+  ref.on('child_removed', (data) => {
     const action = updateCheckIn(data.key, false);
     store.dispatch(action);
   })
